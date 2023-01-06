65df218a08fc714f2d7bc0f27567ea2f321490c5
Henry Deutsch <henrymdeutsch@gmail.com>
2023-01-06 13:59:47 -0500

    miscellaneous small changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // get elements
-var start_time_label = document.getElementsByClassName("input-text")[0];
-var end_time_label = document.getElementsByClassName("input-text")[1];
-var interval_label = document.getElementsByClassName("input-text")[2];
-var start_time = document.querySelector('#start-time');
-var end_time = document.querySelector('#end-time');
-var interval = document.querySelector('#interval');
-var message = document.querySelector('#message');
-var messageContainer = document.querySelector('#message_container');
+const start_time_label = document.getElementsByClassName("input-text")[0];
+const end_time_label = document.getElementsByClassName("input-text")[1];
+const interval_label = document.getElementsByClassName("input-text")[2];
+const start_time = document.querySelector('#start-time');
+const end_time = document.querySelector('#end-time');
+const interval = document.querySelector('#interval');
+const message = document.querySelector('#message');
+const messageContainer = document.querySelector('#message_container');
 
 // reset values if available
 if (localStorage.getItem('start_time')) {
@@ -23,7 +23,7 @@ $('#form').submit(function (e) {
 
 // print date helper function
 function get_date(str) {
-    var d = new Date(str);
+    const d = new Date(str);
     return (d.getHours() > 12 ? d.getHours() - 12 : d.getHours()) 
     + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) 
     + ":" + (d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds());
@@ -38,18 +38,18 @@ function postfix(start_day) {
 
 // given a string like "12:34", return "12:34 AM" or "12:34 PM"
 function display_time(date_str) {
-    var hr = parseInt(date_str.slice(0, 2)); // will automatically strip leading 0's
-    var min = date_str.slice(3, 5);
-    var am_pm = hr < 12 ? "am" : "pm";
-    var hr_12_hr_time = hr > 12 ? hr - 12 : hr; // can't mod by 12 b/c 12 -> 0
+    const hr = parseInt(date_str.slice(0, 2)); // will automatically strip leading 0's
+    const min = date_str.slice(3, 5);
+    const am_pm = hr < 12 ? "am" : "pm";
+    const hr_12_hr_time = hr > 12 ? hr - 12 : hr; // can't mod by 12 b/c 12 -> 0
     if (parseInt(min) == 0) return `${hr_12_hr_time} ${am_pm}`;
     return `${hr_12_hr_time}:${date_str.slice(3, 5)} ${am_pm}`;
 }
 
 // checks if the given hour and minute is before now
 function date_before_now(start_hr, start_min) {
-    var now = new Date();
-    var date = new Date();
+    const now = new Date();
+    const date = new Date();
     date.setHours(start_hr);
     date.setMinutes(start_min);
     return date < now;
@@ -65,26 +65,26 @@ function log_submit(event) {
     console.log('[updated local storage]');
 
     // set the timer
-    var now = new Date();
-    var start_hr = parseInt(localStorage['start_time'].slice(0, 2));
-    var start_min = parseInt(localStorage['start_time'].slice(3, 5));
-    var start_day = date_before_now(start_hr, start_min) ? now.getDate() + 1 : now.getDate(); // basically, start timer at next available start time.
-    var future = new Date(now.getFullYear(), now.getMonth(), start_day, start_hr, start_min, 0);
-    var ms_until_start = future - Date.now(); // in ms
-    var ms_interval = parseInt(localStorage['interval']) * 60 * 1000; // 1 min is 60*1000 ms
+    const now = new Date();
+    const start_hr = parseInt(localStorage['start_time'].slice(0, 2));
+    const start_min = parseInt(localStorage['start_time'].slice(3, 5));
+    const start_day = date_before_now(start_hr, start_min) ? now.getDate() + 1 : now.getDate(); // basically, start timer at next available start time.
+    const future = new Date(now.getFullYear(), now.getMonth(), start_day, start_hr, start_min, 0);
+    const ms_until_start = future - Date.now(); // in ms
+    const ms_interval = parseInt(localStorage['interval']) * 60 * 1000; // 1 min is 60*1000 ms
     setTimeout( () => { // doesn't currently shut off at stop time
         setInterval( () => {
-            var audio = new Audio("Assets/dream_check.mp3");
+            const audio = new Audio("Assets/dream_check.mp3");
             audio.play();
-            var d = new Date(); // because you didn't have new you fuck
+            const d = new Date(); // because you didn't have new you fuck
             console.log("[played sound at " + get_date(d.toString()) + "]");
         }, ms_interval);
     }, ms_until_start);
 
     // write confirmation message
-    final_msg = `Will play sound every ${localStorage['interval']} min starting ${display_time(localStorage['start_time'])} on the ${start_day}${postfix(start_day)}`; // take out leading 0's from time.
+    const final_msg = `Will play sound every ${localStorage['interval']} min starting ${display_time(localStorage['start_time'])} on the ${start_day}${postfix(start_day)}`; // take out leading 0's from time.
     message.innerText = final_msg;
     messageContainer.classList.remove('hidden');
 }
-form = document.querySelector('#form');
+let form = document.querySelector('#form');
 form.addEventListener('submit', log_submit);
